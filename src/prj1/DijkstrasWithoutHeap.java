ada3420b8e7b69da6c3ff884082ad194e8bac882
WilkersonJJ <103959258+WilkersonJJ@users.noreply.github.com>
2023-02-21 12:44:09 -0500

    Initial commit

---
+++ src/prj1/DijkstrasWithoutHeap.java
@@ -0,0 +1,43 @@
+package prj1;
+
+/**
+ * The implementation of Dijkstras shortest path algorithm by using a simple
+ * linear search to find the unvisited node with the minimum distance estimate
+ * 
+ * @author Enter your names here
+ * @version 1.1
+ */
+public class DijkstrasWithoutHeap {
+
+    /**
+     * Constructor of the class
+     * 
+     * @param n:
+     *            number of nodes of the graph
+     * @param edges:
+     *            the set of edges of the graph. Each row of 'edges' is in the
+     *            form of [u, v, w], which means that there is an edge between u
+     *            and v with weight w. So edges[i][0] and edges[i][1] are the
+     *            end-points of the i-th edge and edges[i][2] is its weight
+     */
+    public DijkstrasWithoutHeap(int n, int[][] edges) {
+        // TODO complete
+    }
+
+
+    /**
+     * This method computes and returns the distances of all nodes of the graph
+     * from the source node
+     * 
+     * @param source
+     * 
+     * @return an array containing the distances of the nodes of the graph from
+     *         source. Element i of the returned array represents the distance
+     *         of node i from the source
+     */
+    public int[] run(int source) {
+        // TODO Complete
+        return null;
+    }
+
+}
---
+++ src/prj1/MinHeap.java
@@ -0,0 +1,98 @@
+package prj1;
+
+/**
+ * In this class, we implement the d-ary min-heap data structure
+ * 
+ * @author Jasper Wilkerson
+ * @author Jade Sherer
+ *
+ */
+public class MinHeap {
+    // The parameter d in the d-ary min-heap
+    private int d;
+
+    // The array representation of your min-heap (It is not required to use this)
+    private HeapNode[] nodes;
+
+    /**
+     * Constructor
+     * 
+     * @param n:
+     *            maximum number of elements in the min-heap at each time
+     * @param d:
+     *            parameter d in the d-ary min-heap
+     */
+    public MinHeap(int n, int d) {
+        // TODO complete
+    }
+
+
+    /**
+     * This method inserts a new element with "id" and "value" into the min-heap
+     * 
+     * @param id
+     * @param value
+     */
+    public void insert(int id, int value) {
+        // TODO complete
+    }
+
+
+    /**
+     * This method extracts the min value of the heap
+     * 
+     * @return an array consisting of two integer elements: id of the minimum
+     *         element and the value of the minimum element
+     * 
+     *         So for example, if the minimum element has id = 5 and value = 1,
+     *         you should return the array [5, 1]
+     */
+    public int[] extractMin() {
+        // TODO complete
+        return null;
+    }
+
+
+    /**
+     * This method takes an id and a new value newValue for the corresponding
+     * node, and updates the data structure accordingly
+     * 
+     * @param id
+     * @param newValue
+     */
+    public void decreaseKey(int id, int newValue) {
+        // TODO complete
+    }
+
+
+    /**
+     * This method returns the array representation of heap
+     * 
+     * @return the array representation of heap
+     */
+    public int[] getHeap() {
+        // TODO complete
+        return null;
+    }
+
+
+    /**
+     * the toString method that returns a string with the values of the heap in
+     * the array representation.
+     * This method can help you find the issues of your code when you want to
+     * debug.
+     * 
+     * @return string form of the array representation of heap
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < this.nodes.length; i++) {
+            if (nodes[i] != null) {
+                sb.append(nodes[i].getValue());
+                sb.append(' ');
+            }
+        }
+        return sb.toString();
+    }
+
+}
